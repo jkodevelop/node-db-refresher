0a3e812116de925865d48fac73a85c5ce1ac3fbe
jasonko-home <jasonko@sb2.ca>
2020-08-17 01:14:15 -0400

    step 3: express-swagger-generator setup 1. npm -i --save-dev express-swagger-generator 2. simple options reference for swagger generator 3. write correct API comments (example in basic.js) go to http://localhost:3001/api-docs to see swagger definition note: do not have route that uses that signature

--- routes/basic.js
+++ routes/basic.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const r = express.Router();
 
+/**
+ * This API returns text/html "Hello!"
+ * @route GET /api
+ * @group basic - Basic API
+ * @returns {Error}  default - Unexpected error
+ */
 r.get('/', function(req, res) {
   res.send('Hello!');
 });
--- server.js
+++ server.js
@@ -2,6 +2,39 @@ const express = require('express');
 const app = express();
 const routerConf = require('./routes/index.js');
 
+////////////////////////////////////////////////////////////////
+// express-swagger-generator configurations
+var expressSwagger = require('express-swagger-generator')(app);
+let options = {
+  swaggerDefinition: {
+    info: {
+      description: 'This is a sample server',
+      title: 'Swagger',
+      version: '1.0.0',
+    },
+    host: 'localhost:3001',
+    basePath: '/',
+    produces: [
+      "application/json",
+      "application/xml"
+    ],
+    schemes: ['http', 'https'],
+    securityDefinitions: {
+      JWT: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'x-access-token',
+        description: "",
+      }
+    }
+  },
+  basedir: __dirname, //app absolute path
+  files: ['./routes/**/*.js'] //Path to the API handle folder
+};
+expressSwagger(options);
+// END OF: express-swagger-generator configurations
+////////////////////////////////////////////////////////////////
+
 routeConfig(app);
 
 app.listen(3001);

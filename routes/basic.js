f8826548994bc14ed82888ea228d6838d5254ce2
jasonko-home <jasonko@sb2.ca>
2020-08-17 12:46:49 -0400

    step 4: base GET methods handling examples: 1. no parameters 2. url query parameters (/api?email=xxx&pass=yyy) 3. path parameters (/api/param/:paramValue/other/:otherValue)

--- routes/basic.js
+++ routes/basic.js
@@ -11,4 +11,32 @@ r.get('/', function(req, res) {
   res.send('Hello!');
 });
 
+/**
+ * This is allows you to pass in a url query variable(s)
+ * @route GET /api/urlquery
+ * @group basic - Basic API
+ * @param {string} email.query.required - username or email
+ * @param {string} password.query.required - user's password.
+ * @returns {Error}  default - Unexpected error
+ */
+r.get('/urlquery', function(req, res) {
+  var email = req.query.email;
+  var pass = req.query.password;
+  res.send(`you sent ${email} ${pass}`);
+});
+
+
+
+/**
+ * This allows you to pass in a PATH variable 
+ * @route GET /api/pathvariable/{val}
+ * @group basic - Basic API
+ * @param {string} val.path.required - value of parameter
+ * @returns {Error}  default - Unexpected error
+ */
+r.get('/pathvariable/:val', function(req, res) {
+  var val = req.params.val;
+  res.send(`this is the value "${val}" of the param passed in!`);
+});
+
 module.exports = r;

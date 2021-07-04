4a67ac04fde5c0621fb46afba193f03a114a27c0
jasonko-home <jasonko@sb2.ca>
2021-07-04 02:06:46 -0400

    [MySQL] added create/update/delete examples - added route to POST -> create user in mysql - added route to PUT -> update user in mysql - added route to DELETE -> delete user in mysql
    - a little cleanup

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,13 @@ const mysql = require('mysql2/promise');
 
 const { mysqlConf } = require('../_config.js');
 
+const mysqlOptions = {
+      host: mysqlConf.host, 
+      user: mysqlConf.user, 
+      password: mysqlConf.password,
+      database: mysqlConf.database,
+    };
+
 /**
  * Get newest user
  * @route GET /api/users/latest
@@ -14,12 +21,7 @@ const { mysqlConf } = require('../_config.js');
 r.get('/users/latest', async (req, res) => {
   let users;
   try{
-    const connection = await mysql.createConnection({
-      host: mysqlConf.host, 
-      user: mysqlConf.user, 
-      password: mysqlConf.password,
-      database: mysqlConf.database,
-    });
+    const connection = await mysql.createConnection(mysqlOptions);
 
     const [rows, fields] = await connection.execute('SELECT * FROM `users` ORDER BY id DESC LIMIT 1');
 
@@ -33,7 +35,6 @@ r.get('/users/latest', async (req, res) => {
     console.log(ex);
   } finally {
     console.log('finish');
-
   }
 
   // console.log('request time: ',req.requestTime);
@@ -53,12 +54,7 @@ r.get('/users/get/:id', async (req, res) => {
   const id = req.params.id;
   let users;
   try{
-    const connection = await mysql.createConnection({
-      host: mysqlConf.host, 
-      user: mysqlConf.user, 
-      password: mysqlConf.password,
-      database: mysqlConf.database,
-    });
+    const connection = await mysql.createConnection(mysqlOptions);
 
     const [rows, fields] = await connection.query(
       'SELECT * FROM `users` WHERE `id` = ?',
@@ -75,7 +71,6 @@ r.get('/users/get/:id', async (req, res) => {
     console.log(ex);
   } finally {
     console.log('finish');
-
   }
 
   // console.log('request time: ',req.requestTime);
@@ -83,4 +78,125 @@ r.get('/users/get/:id', async (req, res) => {
 
 });
 
+/**
+ * Create user
+ * @route POST /api/users
+ * @group Users - Users Management
+ * @param {User.model} user.body.required - User object
+ * @returns {Error}  default - Unexpected error
+ * @security JWT
+ */
+r.post('/users', async (req, res) => {
+
+  let postBody = req.body;
+  let insertUser = {
+    'email': postBody.email ? postBody.email : '',
+    'firstname': postBody.firstname ? postBody.firstname : '',
+    'lastname': postBody.lastname ? postBody.lastname : '',
+  };
+
+  let resp;
+  try{
+    const connection = await mysql.createConnection(mysqlOptions);
+
+    const opsRes = await connection.query(
+      'INSERT INTO `users` (`email`, `first_name`, `last_name`) VALUES (?,?,?)',
+      [insertUser.email, insertUser.firstname, insertUser.lastname],
+    );
+
+    console.log('result:', opsRes);
+    resp = { 'insertId': opsRes[0].insertId }
+    connection.end();
+
+  } catch(ex) {
+    console.log(ex);
+  } finally {
+    console.log('finish');
+  }
+
+  // console.log('request time: ',req.requestTime);
+  res.json(resp);
+});
+
+/**
+ * Update user by id
+ * @route PUT /api/users/{id}
+ * @group Users - Users Management
+ * @param {integer} id.path.required - id of user
+ * @param {User.model} user.body.required - User object
+ * @returns {Error}  default - Unexpected error
+ * @security JWT
+ */
+r.put('/users/:id', async (req, res) => {
+
+  let postBody = req.body;
+  let editUser = {
+    'email': postBody.email ? postBody.email : '',
+    'firstname': postBody.firstname ? postBody.firstname : '',
+    'lastname': postBody.lastname ? postBody.lastname : '',
+  };
+  const id = req.params.id;
+
+  let resp;
+  try{
+    const connection = await mysql.createConnection(mysqlOptions);
+
+    const opsRes = await connection.query(
+      'UPDATE `users` SET `email`=?, `first_name`=?, `last_name`=? WHERE `id`=?',
+      [editUser.email, editUser.firstname, editUser.lastname, id],
+    );
+    console.log('result:', opsRes);
+    resp = { 'changedRows': opsRes[0].changedRows }
+    connection.end();
+
+  } catch(ex) {
+    console.log(ex);
+  } finally {
+    console.log('finish');
+  }
+
+  // console.log('request time: ',req.requestTime);
+  res.json(resp);
+});
+
+/**
+ * Delete user by id
+ * @route DELETE /api/users/{id}
+ * @group Users - Users Management
+ * @param {integer} id.path.required - id of user
+ * @returns {Error}  default - Unexpected error
+ * @security JWT
+ */
+r.delete('/users/:id', async (req, res) => {
+
+  const id = req.params.id;
+  let resp;
+  try{
+    const connection = await mysql.createConnection(mysqlOptions);
+
+    const opsRes = await connection.query(
+      'DELETE FROM `users` WHERE `id`=?',
+      [id],
+    );
+    console.log('result:', opsRes);
+    resp = { 'affectedRows': opsRes[0].affectedRows }
+    connection.end();
+
+  } catch(ex) {
+    console.log(ex);
+  } finally {
+    console.log('finish');
+  }
+
+  // console.log('request time: ',req.requestTime);
+  res.json(resp);
+});
+
 module.exports = r;
+
+/**
+ * @typedef User
+ * @property {string} email.required - email of user
+ * @property {string} firstname.required - first name
+ * @property {string} lastname.required - last name
+ */
